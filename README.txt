c857bd403c639aa9d83e75c40888380bc73c140a
Jack Hung <hungjk@gmail.com>
2010-04-19 12:54:30 +0800

    initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+- run with embedded glassfish and build-in javaDB
+  - add src/test/resources/conf/domain.xml and change src/main/resources/META-INF/persistence.xml
+- added repository and dependencies for Zk
+
+mvn embedded-glassfish:run
+
+
---
+++ src/test/java/jh/jee/jh/jee/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package jh.jee.jh.jee;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest
+{
+   @Test
+   public void testGetText() {
+      HelloWorld fixture = new HelloWorld();
+      assert "Hello World!".equals(fixture.getText());
+   }
+}
